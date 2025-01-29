28cdfdbef405dce94067f45a0232f9f3b4e030d0
ldt1997 <474376875@qq.com>
2025-01-29 16:12:33 +0100

    fix: change styles of ProductItem

--- src/components/ProductItem.vue
+++ src/components/ProductItem.vue
@@ -1,30 +1,53 @@
 <template>
-  <b-list-group-item>
-    <img alt="Product" src="../assets/game.png" />
-    {{ product.name }} - ( {{ product.price }}kr )
-    <b-button
-      type="button"
-      variant="link"
-      @click="$emit('order-product', product._id)"
-    >
-      <img alt="Add To Cart" src="../assets/addtocart.jpg" />
-    </b-button>
-    <b-button
-      type="button"
-      variant="link"
-      @click="removeFavorite(product._id)"
-      v-if="favorites?.includes(product._id)"
-    >
-      <img alt="remove Favorite" src="../assets/bag-heart.svg" />
-    </b-button>
-    <b-button
-      type="button"
-      variant="link"
-      @click="addFavorite(product._id)"
-      v-if="!favorites?.includes(product._id)"
-    >
-      <img alt="add to Favorite" src="../assets/bag-heart-fill.svg" />
-    </b-button>
+  <b-list-group-item
+    class="d-flex align-items-center justify-content-between w-"
+  >
+    <div class="d-flex gap-3">
+      <img
+        alt="Product"
+        src="../assets/game.png"
+        style="width: 48px; height: auto"
+      />
+      <div class="text-start">
+        <h5 class="mb-1 fw-bold">{{ product.name }}</h5>
+        <!-- <small>{{ product?._id }}</small> -->
+        <small>{{ product?.category }}</small>
+      </div>
+    </div>
+    <div class="d-flex gap-3 align-items-center">
+      <h3 class="fw-bold m-0">{{ product.price }} kr</h3>
+      <div>
+        <b-button
+          type="button"
+          variant="link"
+          v-b-tooltip.hover
+          title="Add To Cart"
+          @click="$emit('order-product', product._id)"
+        >
+          <img alt="Add To Cart" src="../assets/addtocart.svg" />
+        </b-button>
+        <b-button
+          type="button"
+          variant="link"
+          @click="removeFavorite(product._id)"
+          v-b-tooltip.hover
+          title="Remove From Favorite"
+          v-if="favorites?.includes(product._id)"
+        >
+          <img alt="remove Favorite" src="../assets/bag-heart-fill.svg" />
+        </b-button>
+        <b-button
+          type="button"
+          variant="link"
+          @click="addFavorite(product._id)"
+          v-b-tooltip.hover
+          title="Add To Favorite"
+          v-if="!favorites?.includes(product._id)"
+        >
+          <img alt="add to Favorite" src="../assets/bag-heart.svg" />
+        </b-button>
+      </div>
+    </div>
   </b-list-group-item>
 </template>
 
@@ -61,7 +84,7 @@ export default {
 
 <style scoped>
 img {
-  width: 33px;
-  margin-right: 10px;
+  width: 24px;
+  object-fit: contain;
 }
 </style>
