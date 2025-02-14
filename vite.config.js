27dd2f4831cb6504aef06b650503558f6135b7da
Liafonx <liafonx@outlook.com>
2025-02-14 16:23:25 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -19,6 +19,7 @@ export default defineConfig({
   },
   preview: {
     host: '0.0.0.0',
-    port: process.env.FRONTEND_PORT || 5046
+    port: process.env.FRONTEND_PORT || 5046,
+    allowedHosts: true
   }
 })
