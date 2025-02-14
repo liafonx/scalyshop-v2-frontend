af585da87b8fae96ede1ab276ab1f38e22b5f787
ldt1997 <474376875@qq.com>
2025-02-14 15:36:12 +0100

    fix(ci): change ci config

--- build/build.sh
+++ build/build.sh
@@ -1,26 +1,11 @@
 #!/bin/bash
+set -e  # Stop on errors
 
-# Stop on errors
-set -e
+echo "Building Docker image: ${CI_REGISTRY_IMAGE}:${CI_APPLICATION_TAG}"
 
-# Set Docker image name and image tag
-DOCKER_IMAGE_NAME=${CI_REGISTRY_IMAGE}
-DOCKER_IMAGE_TAG=${CI_APPLICATION_TAG}
+docker build -t ${CI_REGISTRY_IMAGE}:${CI_APPLICATION_TAG} .
 
-# FIXME: echo image name & tag
-echo "DOCKER_IMAGE_NAME: ${DOCKER_IMAGE_NAME}, DOCKER_IMAGE_TAG:${DOCKER_IMAGE_TAG}"
-
-# Run Docker build
-echo "Building Docker image..."
-docker build --provenance=false -t ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG} .
-
-# Log in to Docker registry
-echo "Logging into Docker registry..."
-docker login -u $CI_REGISTRY_USER -p $CI_REGISTRY_PASSWORD $CI_REGISTRY
-
-# Push Docker image
 echo "Pushing Docker image to registry..."
-docker push ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG}
+docker push ${CI_REGISTRY_IMAGE}:${CI_APPLICATION_TAG}
 
-# Echo image tag
-echo "Docker image pushed successfully: ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG}"
+echo "Docker image pushed successfully: ${CI_REGISTRY_IMAGE}:${CI_APPLICATION_TAG}"
